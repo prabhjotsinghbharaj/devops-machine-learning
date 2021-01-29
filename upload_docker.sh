b06874fa03fd833c1547cdb3dfb6d50f4f63bfa6
Prabhjot Singh Bharaj <prabhjot.s.bharaj@mechademy.com>
2021-01-30 02:56:42 +0530

    Add to repo

---
+++ upload_docker.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath="pbharaj1994/udaml"
+
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag udaml $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker image push $dockerpath
